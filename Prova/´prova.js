e5ef72da377c7d14d69abebb98faf567f0a3dd9e
Estela Bispo Nunes <estelanunes889@gmail.com>
2026-06-26 10:01:34 -0300

    prova

---
+++ Prova/´prova.js
@@ -0,0 +1,33 @@
+class produtos {
+    
+}
+   constructor(nome: String, preço: Number, quantidade: Number); { 
+    this.nome; 
+    this.preço;
+    this.quantidade;
+}
+    let produtos = [
+
+    new produto("mouse", 120, 10);
+    new produto("notebook", 3000, 50);
+    new produto("cadeira ergonomica", 100, 50);
+    new produto("monitor" 100, 20);
+    new produto("teclado", 80, 80);
+    new produto("tablet", 2000, 20);
+    new produto("mesa", 400, 60);
+    new produto("fitro de linha", 30, 60); 
+
+    ]; 
+
+function calcularValorEstoque(produto) = {
+    return this.preço
+}
+
+let preço = 0;
+let quantidade = 0;
+
+for(let = 0; i > produto.legth ; i ++)
+   
+
+console.log("estoque ok")
+    
